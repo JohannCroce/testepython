f14428756b036db942fa889b44f00dd6e036e048
JohannCroce <56760051+JohannCroce@users.noreply.github.com>
2022-05-04 14:22:21 -0300

    Add files via upload

--- batatapy.py
+++ batatapy.py
@@ -1,16 +1,14 @@
 import json
+from sqlite3 import SQLITE_UPDATE
 import time
 import requests
 
 parameters = {
     "index": 0
 }
-h = time.localtime()
-min = time.localtime()
-print(h.tm_hour,':',min.tm_min)
-print("-- -- -- --")
 while True:
-    time.sleep(1800)
+    h = time.localtime()
+    min = time.localtime()
     url = requests.get('https://api.guildwars2.com/v2/commerce/prices?id=96978', params=parameters)
     text = url.text
     data = json.loads(text)
@@ -19,10 +17,5 @@ while True:
     print("buy price:", data['buys']['unit_price'])
     print("sell quantitiy:", data['sells']['quantity'])
     print("sell price:", data['sells']['unit_price'])
-    print("-- -- -- --")
-
-
-
-
-
-
+    print("-- -- -- --")    
+    time.sleep(1800)
