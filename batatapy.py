0ef89c4205bb1985c87f25a5675aeb38b16bcdab
JohannCroce <56760051+JohannCroce@users.noreply.github.com>
2022-05-04 09:33:26 -0300

    Add files via upload

--- batatapy.py
+++ batatapy.py
@@ -5,17 +5,24 @@ import requests
 parameters = {
     "index": 0
 }
-
+h = time.localtime()
+min = time.localtime()
+print(h.tm_hour,':',min.tm_min)
+print("-- -- -- --")
 while True:
-    min = time.localtime()
-    if min.tm_min == 0 or min.tm_min == 30:
-        print(min.tm_min)
-        url = requests.get('https://api.guildwars2.com/v2/commerce/prices?id=96978', params=parameters)
-        text = url.text
-        data = json.loads(text)
-        print(data['buys'])
-        print(data['sells'])
-    else:
-        print(min.tm_min)
-        print("not yet time!")
-    time.sleep(60)
+    time.sleep(1800)
+    url = requests.get('https://api.guildwars2.com/v2/commerce/prices?id=96978', params=parameters)
+    text = url.text
+    data = json.loads(text)
+    print(h.tm_hour,':',min.tm_min)
+    print("buy quantity:", data['buys']['quantity'])
+    print("buy price:", data['buys']['unit_price'])
+    print("sell quantitiy:", data['sells']['quantity'])
+    print("sell price:", data['sells']['unit_price'])
+    print("-- -- -- --")
+
+
+
+
+
+
